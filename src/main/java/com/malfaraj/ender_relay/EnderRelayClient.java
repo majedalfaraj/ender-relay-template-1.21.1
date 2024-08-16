ff71aa47d1e00d44b2754a52cc659716795afec4
Majed AlFaraj <malfaraj@berkeley.edu>
2024-08-15 22:55:36 -0700

    Initial commit

---
+++ src/main/java/com/malfaraj/ender_relay/EnderRelayClient.java
@@ -0,0 +1,11 @@
+package com.malfaraj.ender_relay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderRelayClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/malfaraj/ender_relay/EnderRelayDataGenerator.java
@@ -0,0 +1,11 @@
+package com.malfaraj.ender_relay;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnderRelayDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
